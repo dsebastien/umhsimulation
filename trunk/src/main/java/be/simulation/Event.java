f012888163a30d8e5a4a678b4d4d3ec27fdc780d
lechtitseb <lechtitseb@59f556cc-09d1-11de-b321-f573fdaff03e>
2009-03-05 22:49:42 +0000

    Initial commit

---
+++ trunk/src/main/java/be/simulation/Event.java
@@ -0,0 +1,30 @@
+package be.simulation;
+
+
+public class Event {
+	private long time;
+	private EventType eventType; 
+	
+	public Event(long time, EventType eventType){
+		this.time = time;
+		this.eventType = eventType;
+	}
+	
+	
+	
+	public long getTime() {
+		return time;
+	}
+
+	public void setTime(long time) {
+		this.time = time;
+	}
+
+	public EventType getEventType() {
+		return eventType;
+	}
+
+	public void setEventType(EventType eventType) {
+		this.eventType = eventType;
+	}
+}
---
+++ trunk/src/main/java/be/simulation/EventType.java
@@ -0,0 +1,6 @@
+package be.simulation;
+
+public enum EventType {
+	ARRIVEE,
+	DEPART
+}
