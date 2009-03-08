67dc7b49c6a27154270bfec539cfc7fae505b570
lechtitseb <lechtitseb@59f556cc-09d1-11de-b321-f573fdaff03e>
2009-03-08 19:54:28 +0000

    Finished tp1 and updated the abstract simulation class

--- trunk/src/main/java/be/simulation/tp1/model/NewsDealer.java
+++ trunk/src/main/java/be/simulation/tp1/model/NewsDealer.java
@@ -3,108 +3,134 @@ package be.simulation.tp1.model;
 import java.util.Random;
 
 public class NewsDealer {
+	private final Random	demandGenerator			= new Random();
+	private final Random	dayTypeGenerator		= new Random();
+	private final double	buyPrice				= 0.33;
+	private final double	sellPrice				= 0.5;
+	private final double	scrapPrice				= 0.05;
+	private final double	profitPerNewspaper		= sellPrice - buyPrice;
+	private final int		newspapersPerDay;
+	private int				simulatedDays			= 0;
+	private double			totalProfit				= 0;
+	private long			totalAskedNewspapers	= 0;
 
-	private final double buyPrice;
-	private final double sellPrice;
-	private final double profitPerNewspaper;
-	private final double scrapPrice;
-	private final long newspapersPerDay;
-	private final long simulatedDays = 0;
-	private double totalProfit;
-	private long totalAskedNewspapers = 0;
-	private final Random demandGenerator;
 
-	public NewsDealer(long newsPapersPerDay) {
+
+	public NewsDealer(int newsPapersPerDay) {
 		this.newspapersPerDay = newsPapersPerDay;
-		demandGenerator = new Random();
-		buyPrice = 0.33;
-		sellPrice = 0.5;
-		scrapPrice = 0.05;
-		profitPerNewspaper = sellPrice - buyPrice;
 	}
 
-	public void runOneDay(DayType dayType) {
-		// FIXME implement
-		// generate the number of sold news papers for this day
-		// FIXME based on the bought news papers number, use proba to see how
-		// much we gain & store the results
 
-		// int newstypeR = type.nextInt(100)+1;
-		// int newsdemandR = demande.nextInt(100)+1;
-		// int qtty=0;
-		//	        
-		// if (newstypeR <=35) { // good
-		//	            
-		// if (newsdemandR <=3)
-		// qtty = 40;
-		// else if (newsdemandR <=8)
-		// qtty = 50;
-		// else if (newsdemandR <=23)
-		// qtty = 60;
-		// else if (newsdemandR <= 43)
-		// qtty = 70;
-		// else if (newsdemandR <=78)
-		// qtty = 80;
-		// else if (newsdemandR <= 93)
-		// qtty = 90;
-		// else //if (newsdemandR <=100)
-		// qtty = 100;
-		//	            
-		// }else if (newstypeR <=80 ) { // fair
-		//	            
-		// if (newsdemandR <=10)
-		// qtty = 40;
-		// else if (newsdemandR <=28)
-		// qtty = 50;
-		// else if (newsdemandR <=68)
-		// qtty = 60;
-		// else if (newsdemandR <= 88)
-		// qtty = 70;
-		// else if (newsdemandR <=96)
-		// qtty = 80;
-		// else //if (newsdemandR <= 100)
-		// qtty = 90;
-		//	            
-		// }else //if (newstypeR <=100) { // poor
-		// {
-		// if (newsdemandR <=44)
-		// qtty = 40;
-		// else if (newsdemandR <=66)
-		// qtty = 50;
-		// else if (newsdemandR <=82)
-		// qtty = 60;
-		// else if (newsdemandR <= 94)
-		// qtty = 70;
-		// else // if (newsdemandR <=100)
-		// qtty = 80;
-		// }
-		//	        
-		//	        
-		// this.totalNewsAsked += qtty;
-		// int nbrVendu = Math.min(qtty,orderedQtty);
-		// int nbrLost = Math.max(0,qtty-orderedQtty);
-		// int nbrScrap = Math.max(0,orderedQtty-qtty);
-		// nbRun++;
-		// double dailyProfit = (PVENTE*nbrVendu) - (orderedQtty*PACHAT) -
-		// (PROFIT*nbrLost) + (PSCRAP*nbrScrap);
-		//	   
-		// totalProfit+= dailyProfit;
-		// return dailyProfit;
+
+	/**
+	 * Generates a random day type.
+	 * 
+	 * @return the generated day type
+	 */
+	private DayType generateDayType() {
+		// Good: proba: 0.35 cumulative: 0.35 digits: 01-35
+		// Fair: proba: 0.45 cumulative: 0.80 digits: 36-80
+		// Poor: proba: 0.20 cumulative: 1.00 digits: 81-00
+		int dayType = dayTypeGenerator.nextInt(100) + 1;
+		DayType returnValue = null;
+		if (dayType <= 35) {
+			returnValue = DayType.GOOD;
+		} else if (dayType <= 80) {
+			returnValue = DayType.FAIR;
+		} else {
+			returnValue = DayType.POOR;
+		}
+		return returnValue;
 	}
 
-	// FIXME implement method to return the results
-	// maybe try something with observable
-	// NewsDealer extends Entity
-	// Entity implements Observable
-	// the simulation registers as Observer
-	// callbacks with simulation step results ...
-	// other interface for simulation for results gathering & so on??
+
+
+	/**
+	 * Generates the newspaper demand based on the day type.
+	 * 
+	 * @param dayType
+	 *        the day type
+	 * @return the newspaper demand
+	 */
+	private int generateNewspaperDemand(DayType dayType) {
+		int returnValue = 0;
+		int randomDemand = demandGenerator.nextInt(100) + 1;
+		// Random digit assignements (book p41)
+		if (dayType == DayType.GOOD) {
+			if (randomDemand <= 3) {
+				returnValue = 40;
+			} else if (randomDemand <= 8) {
+				returnValue = 50;
+			} else if (randomDemand <= 23) {
+				returnValue = 60;
+			} else if (randomDemand <= 43) {
+				returnValue = 70;
+			} else if (randomDemand <= 78) {
+				returnValue = 80;
+			} else if (randomDemand <= 93) {
+				returnValue = 90;
+			} else {
+				returnValue = 100;
+			}
+		} else if (dayType == DayType.FAIR) {
+			if (randomDemand <= 10) {
+				returnValue = 40;
+			} else if (randomDemand <= 28) {
+				returnValue = 50;
+			} else if (randomDemand <= 68) {
+				returnValue = 60;
+			} else if (randomDemand <= 88) {
+				returnValue = 70;
+			} else if (randomDemand <= 96) {
+				returnValue = 80;
+			} else {
+				returnValue = 90;
+			}
+		} else if (dayType == DayType.POOR) {
+			if (randomDemand <= 44) {
+				returnValue = 40;
+			} else if (randomDemand <= 66) {
+				returnValue = 50;
+			} else if (randomDemand <= 82) {
+				returnValue = 60;
+			} else if (randomDemand <= 94) {
+				returnValue = 70;
+			} else {
+				returnValue = 80;
+			}
+		}
+		return returnValue;
+	}
+
+
+
+	public void runOneDay() {
+		// generate the day type
+		DayType dayType = generateDayType();
+		// generate the newspaper demand for the day
+		int newspaperDemand = generateNewspaperDemand(dayType);
+		// update the counters
+		totalAskedNewspapers += newspaperDemand;
+		int soldNewspapers = Math.min(newspaperDemand, newspapersPerDay);
+		int lostNewspapers = Math.max(0, newspaperDemand - newspapersPerDay);
+		int scrapNewspapers = Math.max(0, newspapersPerDay - newspaperDemand);
+		simulatedDays++;
+		double dailyProfit =
+				(sellPrice * soldNewspapers) - (newspapersPerDay * buyPrice)
+						- (profitPerNewspaper * lostNewspapers)
+						+ (scrapPrice * scrapNewspapers);
+		totalProfit += dailyProfit;
+	}
+
+
 
 	public double getAVGProfit() {
 		return totalProfit / simulatedDays;
 	}
 
-	public double getAVGAskedNewspapers() {
+
+
+	public double getAVGNewspaperDemandPerDay() {
 		return (double) totalAskedNewspapers / simulatedDays;
 	}
 }
