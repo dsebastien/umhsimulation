c54667baef71886eae043d75a710e71b0a6a4af6
lechtitseb <lechtitseb@59f556cc-09d1-11de-b321-f573fdaff03e>
2009-04-22 21:43:07 +0000

    Ajout des propriétés (pour les stats) des agents et hotes. Màj de l'uml

--- src/main/java/be/simulation/entites/Hote.java
+++ src/main/java/be/simulation/entites/Hote.java
@@ -9,15 +9,42 @@ package be.simulation.entites;
  */
 public class Hote extends AbstractEntiteSimulationReseau {
 	/**
+	 * Nombre d'accusés de réception reçus.
+	 */
+	private int		accusesReceptionRecus	= 0;
+	/**
 	 * Agent auquel cet hote est relié (pour pouvoir communiquer avec lui).
 	 */
 	private Agent	agent;
+
+	
+	/**
+	 * Le nombre de messages en cours de traitement.
+	 */
+	private int		messagesEnCoursTraitement	= 0;
+	/**
+	 * Nombre de messages envoyés.
+	 */
+	private int		messagesEnvoyes		= 0;
+
+	/**
+	 * Nombre de messages réexpédiés.
+	 */
+	// FIXME à cause de timeouts trop courts?
+	private int		messagesReexpedies	= 0;
+
+
 	/**
 	 * Numero identifiant de l'hote.
 	 */
 	private int		numero;
 
 
+	/**
+	 * Le temps total de voyage des messages.
+	 */
+	private long	tempsTotalVoyageMessages	= 0;
+	
 
 	/**
 	 * Créer un nouvel hote.
@@ -39,12 +66,45 @@ public class Hote extends AbstractEntiteSimulationReseau {
 
 
 	/**
+	 * Récupérer le nombre d'accusés de réception reçus.
+	 * 
+	 * @return le nombre d'accusés de réception reçus.
+	 */
+	public int getAccusesReceptionRecus() {
+		return accusesReceptionRecus;
+	}
+
+
+
+	/**
 	 * Récupérer l'agent auquel cet hôte est connecté.
 	 * 
 	 * @return l'agent auquel cet hôte est connecté
 	 */
 	public Agent getAgent() {
 		return agent;
+	}
+
+
+
+	/**
+	 * Récupérer le nombre de messages envoyés.
+	 * 
+	 * @return le nombre de messages envoyés.
+	 */
+	public int getMessagesEnvoyes() {
+		return messagesEnvoyes;
+	}
+
+
+
+	/**
+	 * Récupérer le nombre de messages réexpédiés.
+	 * 
+	 * @return le nombre de messages réexpédiés
+	 */
+	public int getMessagesReexpedies() {
+		return messagesReexpedies;
 	}
 
 
@@ -60,9 +120,25 @@ public class Hote extends AbstractEntiteSimulationReseau {
 
 
 
+	/**
+	 * Récupérer le temps total de voyage des messages.
+	 * 
+	 * @return le temps total de voyage des messages
+	 */
+	public long getTempsTotalVoyageMessages() {
+		return tempsTotalVoyageMessages;
+	}
+
+
+
 	@Override
 	public void reset() {
 		// TODO ici tout remettre à zéro (compteurs, ...)
+		this.messagesEnvoyes = 0;
+		this.messagesReexpedies = 0;
+		this.accusesReceptionRecus = 0;
+		this.tempsTotalVoyageMessages = 0;
+		this.messagesEnCoursTraitement = 0;
 	}
 
 
